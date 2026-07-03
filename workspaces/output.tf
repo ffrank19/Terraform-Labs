ccbede039135921f248f8dc8942d9079daf1d69a
Ubuntu <azureuser@VM-Terraform.w0p5aevfpf3ubkepnh0aiopmud.cx.internal.cloudapp.net>
2026-07-03 02:05:00 +0000

    terraform labs

---
+++ workspaces/output.tf
@@ -0,0 +1,15 @@
+output "instance_size" {
+  value = module.server.instance_size
+}
+
+output "instance_id" {
+  value = module.server.instance_id
+}
+
+output "availability_zone" {
+  value = module.server.availability_zone
+}
+#todo el bloque
+output "instance_name" {
+  value = module.server.instance_name
+}
